7efc673eaec15902b618a17a2f9730bb2fc83f32
James VanderKam <jamesvanderkam@verizon.net>
2021-03-25 17:10:38 -0400

    models, routes begun

--- models/Comment.js
+++ models/Comment.js
@@ -2,5 +2,36 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 // TODO: YOUR CODE HERE
+class Comment extends Model {}
+
+Comment.init(
+    {
+        // define columns
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        body:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        postid:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+        }
+
+    },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -2,5 +2,35 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 // TODO: YOUR CODE HERE
+class Post extends Model {}
+
+Post.init(
+    {
+        // define columns
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        body:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        }
+
+    },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
 
 module.exports = Post;
+
